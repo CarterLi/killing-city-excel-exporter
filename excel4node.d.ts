c9152e59d86f0a75df8ddc020be48c6649c0c59b
CarterLi <tongzhou.li@ele.me>
2016-02-03 17:04:57 +0800

    代码更新

---
+++ excel4node.d.ts
@@ -0,0 +1,190 @@
+declare module 'excel4node' {
+  export interface IWorkBookOption {
+    jszip?: JSZipFileOptions;
+
+    fileSharing?: {
+      userName: string;
+      password: string;
+    }
+  }
+
+  export interface IFont {
+    size?: number;
+    bold?: boolean;
+    italics?: boolean;
+    underline?: boolean;
+    color?: string;
+    font?: string;
+  }
+
+  export interface IWorkSheetOption {
+    margins?: {
+      left: number;
+      right: number;
+      top: number;
+      bottom: number;
+      footer: number;
+      header: number;
+    }
+
+    printOptions?: {
+      centerHorizontal: boolean;
+      centerVertical: boolean;
+    }
+
+    view?: {
+      zoom: number;
+    }
+
+    outline?: {
+      summaryBelow: boolean;
+    }
+
+    fitToPage?: {
+      fitToHeight: number;
+      orientation: string;
+    }
+
+    sheetProtection?: {
+      autoFilter: boolean;
+      deleteColumns: boolean;
+      deleteRow: boolean;
+      formatCells: boolean;
+      formatColumns: boolean;
+      formatRows: boolean;
+      insertColumns: boolean;
+      insertHyperlinks: boolean;
+      insertRows: boolean;
+      objects: boolean;
+      password: string;
+      pivotTables: boolean;
+      scenarios: boolean;
+      sheet: boolean;
+      sort: boolean;
+    }
+  }
+
+  export interface IArea {
+    rows: {
+      begin: number;
+      end: number;
+    }
+
+    columns: {
+      begin: number;
+      end: number;
+    }
+  }
+
+  // A Workbook represents an Excel document.
+  export class WorkBook {
+    constructor(option?: IWorkBookOption)
+
+    WorkSheet(name?: string, option?: IWorkSheetOption): WorkSheet;
+
+    // Set a default font for the workbook
+    updateDefaultFont(font: IFont);
+
+    write(filename: string);
+
+    Style(): IStyle;
+  }
+
+  // A Worksheet represents a tab within an excel document.
+  export class WorkSheet {
+    Cell(startRow: number, startCol: number, endRow?: number, endCol?: number, isMerged?: boolean): ICell;
+
+    // https://support.office.com/en-US/article/Set-a-specific-print-area-BEEBCEB7-0D43-4E07-8895-5AFE0AEDFB32
+    printArea(area: IArea);
+
+    // https://support.office.com/en-us/article/Repeat-specific-rows-or-columns-on-every-printed-page-0d6dac43-7ee7-4f34-8b08-ffcc8b022409
+    printTitles(area: IArea);
+
+    // Uses https://poi.apache.org/apidocs/org/apache/poi/xssf/usermodel/extensions/XSSFHeaderFooter.html
+    headerFooter(options: {
+      firstHeader: string;
+      firstFooter: string;
+      evenHeader: string;
+      evenFooter: string;
+      oddHeader: string;
+      oddFooter: string;
+    });
+
+    Column(index: number): IColumn;
+    Row(index: number): IRow;
+  }
+
+  export interface IFreezable {
+    // Freeze rows and columns
+    Freeze(pos?: number): this;
+  }
+
+  export interface IHideable {
+    // Hide a specific row or column
+    Hide(): this;
+  }
+
+  export interface IRow extends IFreezable, IHideable {
+    // Set a row to be a filter row
+    Filter(startColumn?: number, endColumn?: number): this;
+
+    // Set dimensions of rows or columns
+    Height(length: number): this;
+
+    // Set groupings on rows and optionally collapse them
+    Group(level: number, isCollapsed: number): this;
+  }
+
+  export interface IColumn extends IFreezable, IHideable {
+    // Set dimensions of rows or columns:
+    Width(length: number): this;
+  }
+
+  export interface ICell {
+    String(value: string);
+    Complex(value: any[]);
+    Number(value: number);
+    Formula(value: string);
+    Date(value: Date);
+    Link(value: string, linkName?: string);
+    Bool(value: boolean);
+
+    Style(value: IStyle);
+    Format: IStyle;
+  }
+
+  export interface IStyle {
+    Font: {
+      Bold();
+      Italics();
+      Underline();
+      Family(value: string);
+      Color(value: string);
+      Size(value: number);
+
+      Alignment: {
+        Vertical(value: string);
+        Horizontal(value: string);
+        Rotation(value: string);
+      }
+
+      WrapText(value: boolean);
+    };
+
+    Number: {
+      Format(value: string);
+    };
+
+    Fill: {
+      Pattern(value: string);
+      Color(value: string);
+    };
+
+    Border(
+      top: { style: string; color?: string; },
+      bottom: { style: string; color?: string; },
+      left: { style: string; color?: string; },
+      right: { style: string; color?: string; }
+    )
+  }
+}
--- index.ts
+++ index.ts
@@ -2,16 +2,20 @@
 
 import htmlparser2 = require('htmlparser2');
 import fetch = require('node-fetch');
-import excelExport = require('excel-export');
+import xl = require('excel4node');
 import fs = require('fs');
 
 const domain = 'http://xmdswiki.opd2c.com';
 
 const storage = {
-  total: 1200,
+  total: undefined,
   received: 0,
   parsed: 0
 };
+const errors = {
+  unexpectedError: [],
+  noContent: []
+}
 
 function dfs(dom: any[], condition: (elem) => boolean): any {
   let result;
@@ -47,16 +51,37 @@ function counter(key) {
   process.stdout.write(`Total: ${storage.total}; Received: ${storage.received}; Parsed: ${storage.parsed}; Percentage: ${(storage.received / storage.total * 100).toFixed(2)}%\u001b[0G`);
 };
 
-function logError(msg, detail) {
-  // console.log(msg, detail);
+function logError(msg, err) {
+  // console.error(msg, err);
 }
 
-Promise.all(new Array(storage.total).fill(null)
-  .map((_, index) => fetch(`${domain}/index.php?r=cards%2Fdetail&roleid=${index + 1}`)
+function generateIDs() {
+  const result = new Array(1200 + 1);
+  storage.total = result.length;
+  for (let i = 0; i < 1200; ++i) result[i] = i + 1;
+  result[1200] = 3001;
+  return result;
+}
+
+const titles = [
+  'ID', '名称', '稀有度', '属性', '类型', '念力值', 'MinHP', 'MaxHP', 'MinATK',
+  'MinHeal', 'MaxHeal', 'MaxLV', '技能名称', '技能说明', '技能初始回合数',
+  '技能Max时回合数', '技能最大lv', '队长技能', '队长技能说明', '大插图', '小插图'
+];
+
+Promise.all(generateIDs()
+  .map(ID => fetch(`${domain}/index.php?r=cards%2Fdetail&roleid=${ID}`)
     .then(result => result.text())
+    .catch(err => {
+      errors.unexpectedError.push(ID);
+      throw err;
+    })
     .then(result => {
       counter('received');
-      if (!result) throw new Error('Empty content');
+      if (!result) {
+        errors.noContent.push(ID);
+        throw new Error('服务器返回数据为空（通常代表该ID念灵不存在）')
+      }
       return result;
     })
     .then(html => new Promise<any[]>((resolve, reject) => {
@@ -71,13 +96,11 @@ Promise.all(new Array(storage.total).fill(null)
     }))
     .then(dom => dfs(dom, elem => elem.attribs.id === 'jobs'))
     .then(box => [
-      `${domain}/static/img/pics/big${padLeft(String(index + 1), '0', 4)}.png`,
-      `${domain}/static/img/icons/face${padLeft(String(index + 1), '0', 4)}.png`,
+      ID, // getValueByAttributeName(box, 'ID').children[0].data,
       dfs(box.children, elem => elem.attribs.class === 'bold' && elem.parent.attribs.class === 'headtitle')
           .children[0].data,
       getValueByAttributeName(box, '稀有度').children
           .reduce((prev, curr) => prev + (curr.type === 'tag' && curr.attribs.class === 'level_solid'), 0),
-      index + 1, // getValueByAttributeName(box, 'ID').children[0].data,
       getValueByAttributeName(box, '属性').children[0].data,
       getValueByAttributeName(box, '类型').children[0].data,
       getValueByAttributeName(box, '念力值').children[0].data | 0,
@@ -89,59 +112,87 @@ Promise.all(new Array(storage.total).fill(null)
       getValueByAttributeName(box, 'MaxLV').children[0].data | 0,
       getValueByAttributeName(box, '技能名称').children[0].data,
       getValueByAttributeName(box, '技能说明').children[0].data.trim(),
-      getValueByAttributeName(box, '技能初始回合数').children[0].data,
-      getValueByAttributeName(box, '技能Max时回合数').children[0].data,
-      getValueByAttributeName(box, '技能最大lv').children[0].data,
+      getValueByAttributeName(box, '技能初始回合数').children[0].data | 0 || '--',
+      getValueByAttributeName(box, '技能Max时回合数').children[0].data | 0 || '--',
+      getValueByAttributeName(box, '技能最大lv').children[0].data | 0 || '--',
       getValueByAttributeName(box, '队长技能').children[0].data,
       getValueByAttributeName(box, '队长技能说明').children[0].data.trim(),
-      '', // getValueByAttributeName(box, '队长技能说明').children[0].children[0].data,
-      '', // getValueByAttributeName(box, '进化前念灵ID').children[0].data
+      `${domain}/static/img/pics/big${padLeft(String(ID), '0', 4)}.png`,
+      `${domain}/static/img/icons/face${padLeft(String(ID), '0', 4)}.png`
     ])
     .then(result => {
       counter('parsed');
       return result;
     })
-    .catch(err => logError(`获取念灵 ${index + 1} 信息失败`, err.stack))
+    .catch(err => logError(`获取念灵 ${ID} 信息失败`, err))
   )
 )
 .then(result => result.filter(record => record != null))
 .then(result => {
-  console.log(`\n成功获取到${result.length}条数据，失败${storage.total-storage.received}个，${storage.received-storage.parsed}个请求未返回数据`);
+  console.log(`\n\n成功获取到${result.length}条数据，失败${storage.total-storage.received}个，${storage.received-storage.parsed}个请求未返回数据。`);
+  console.log(`请求失败的念灵：[${errors.unexpectedError.sort().join(', ')}]`);
+  console.log(`返回数据为空的念灵（通常代表该ID念灵不存在）：[${errors.noContent.sort().join(', ')}]`);
   return result;
 })
-.then(result => excelExport.execute({
-  cols: [
-    { caption: '大插图', type: 'string' },
-    { caption: '小插图', type: 'string' },
-    { caption: '名称', type: 'string' },
-    { caption: '稀有度', type: 'number' },
-    { caption: 'ID', type: 'number' },
-    { caption: '属性', type: 'string' },
-    { caption: '类型', type: 'string' },
-    { caption: '念力值', type: 'number' },
-    { caption: 'MinHP', type: 'number' },
-    { caption: 'MaxHP', type: 'number' },
-    { caption: 'MinATK', type: 'number' },
-    { caption: 'MinHeal', type: 'number' },
-    { caption: 'MaxHeal', type: 'number' },
-    { caption: 'MaxLV', type: 'number' },
-    { caption: '技能名称', type: 'string' },
-    { caption: '技能说明', type: 'string' },
-    { caption: '技能初始回合数', type: 'string' },
-    { caption: '技能Max时回合数', type: 'string' },
-    { caption: '技能最大lv', type: 'string' },
-    { caption: '队长技能', type: 'string' },
-    { caption: '队长技能说明', type: 'string' },
-    { caption: '进化后念灵名', type: 'string' },
-    { caption: '进化前念灵ID', type: 'string' }
-  ],
-  rows: result
-}))
-.then(content => new Promise((resolve, reject) => {
-  fs.writeFile('killing-city.xlsx', content, 'binary', err => {
-    if (err) reject(err);
-    resolve();
-  })
-}))
-.then(() => console.log('killing-city.xlsx：文件成功生成'))
-.catch(err => logError('生成文件失败', err));
+.then(result => {
+  const workbook = new xl.WorkBook();
+  workbook.updateDefaultFont({
+    size: 14,
+    font: '微软雅黑'
+  });
+
+  const worksheet = workbook.WorkSheet('念灵图鉴');
+
+  titles.forEach((title, columnIndex) => worksheet.Cell(1, columnIndex + 1).String(title));
+
+  result.forEach((row, rowIndex) => {
+    row.forEach((cell, columnIndex) => {
+      const workCell = worksheet.Cell(rowIndex + 2, columnIndex + 1);
+
+      if (columnIndex >= titles.length - 2) {
+        // 图片链接
+        workCell.Link(cell);
+      } else {
+        switch (typeof cell) {
+        case 'number':
+          workCell.Number(cell);
+          break;
+        case 'boolean':
+          workCell.Bool(cell);
+          break;
+        case 'object':
+          if (cell instanceof Date) {
+            workCell.Date(cell);
+            break;
+          }
+          // Fall through
+        default:
+          workCell.String(String(cell));
+          break;
+        }
+      }
+    });
+  });
+
+  {
+    const linkStyle = workbook.Style();
+    linkStyle.Font.Color('#0077CC');
+    linkStyle.Font.Underline();
+    worksheet.Cell(2, titles.length - 1, result.length + 1, titles.length).Style(linkStyle);
+    worksheet.Cell(1, 1, 1, titles.length).Format.Font.Bold();
+    worksheet.Cell(1, 1, result.length + 1, titles.length).Format.Font.Alignment.Horizontal('center');
+  }
+
+  worksheet.printTitles({
+    rows: { begin: 1, end: result.length + 1 },
+    columns: { begin: 1, end: titles.length + 1 }
+  });
+
+  worksheet.Row(1).Filter(1, titles.length - 2);
+  worksheet.Row(2).Freeze();
+  worksheet.Column(2).Freeze();
+
+  workbook.write('killing-city.xlsx');
+})
+.then(() => console.log('\nkilling-city.xlsx：文件成功生成'))
+.catch(err => console.error('\nFATAL：生成文件失败', err));
